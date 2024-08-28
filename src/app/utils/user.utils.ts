dfe9b2fb21afc3e47acf9af1c88b991221c9c88f
Rakib hosen <rakibhosen2244@gmail.com>
2024-08-28 13:22:57 +0600

    this is code and year crate

--- src/app/utils/user.utils.ts
+++ src/app/utils/user.utils.ts
@@ -13,12 +13,32 @@ const findLastStudentId = async () => {
   )
     .sort({ id: -1 })
     .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined; // 0001
+  return lastStudent?.id ? lastStudent.id : undefined; // 0001
 };
 
 export const generateStudentId = async (payload: IAcademicSemester) => {
   // console.log(await findLastStudentId());
-  const currentId = (await findLastStudentId()) || (0).toString();
+
+  let currentId = (0).toString(); // 0000 by default
+
+  const lastStudentId = await findLastStudentId();
+  // 2024 02 0002
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  console.log("Least Student Semester Code", lastStudentSemesterCode);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  console.log("Last Student Year ", lastStudentYear);
+  const currentSemeterCode = payload.code;
+  console.log("Current Semeter Code ", currentSemeterCode);
+  const currentYear = payload.year;
+  console.log("Current Year ", currentYear);
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemeterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
   let incrementId = (parseInt(currentId) + 1).toString().padStart(4, "0");
   return `${payload.year}${payload.code}${incrementId}`;
 };
